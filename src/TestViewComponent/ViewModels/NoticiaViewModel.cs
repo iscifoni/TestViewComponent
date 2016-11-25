3c56a6b2cf0c073b8eee8f4c5a18093a47b2f647
José Manuel Sánchez Simón <josess@balideacp.local>
2016-11-25 09:08:26 +0100

    Initial comit

---
+++ src/TestViewComponent/ViewModels/NoticiaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace TestViewComponent.ViewModels
+{
+    public class NoticiaViewModel 
+    {
+        private IList<ArchivoViewModel> _archivos = new List<ArchivoViewModel>();
+
+        public IList<ArchivoViewModel> Archivos { get { return _archivos; } set { _archivos = value; } }
+
+        public NoticiaViewModel()
+        {
+        }
+    }
+}
